610719a7068ddc10a0e5f59439b75e7800a13913
Rudi Starcevic <ooly.me@gmail.com>
2018-01-12 17:13:27 +0800

    Add platorm secrets

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,21 @@ const Lynx = require('lynx')
 
 const metrics = new Lynx('telegraf', 8125)
 
+// secrets
+const fs = require('fs')
+const dbUsername = fs.readFileSync('/run/secrets/database_username')
+const dbPassword = fs.readFileSync('/run/secrets/database_password')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   metrics.increment('service.home')
-  res.render('index', { title: 'Viseo Hong Kong - Digital Innovation Labs', hostname: os.hostname, timestamp: new Date().toLocaleString() });
+  res.render('index', { 
+    title: 'Viseo Hong Kong - Digital Innovation Labs', 
+    hostname: os.hostname, 
+    timestamp: new Date().toLocaleString(),
+    dbUsername,
+    dbPassword
+   });
 });
 
 module.exports = router;
